468a53e53cbc1c2f15d4d09d57a40510034f3c2c
Fabio Andrade <fmandrade2319@gmail.com>
2020-07-09 09:14:27 -0300

    Implementados metodos geter na definição da classe Negociacao

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,8 +1,32 @@
 class Negociacao {
 
+    //Javascript ainda não proporciona restrição de acesso em OO então utilizamos a convenção
+    //do underline para deixar claro que as propriedades não devem ser acessadas diretamente.
+
     constructor(data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    //javascript oferece o geter com essa sintaxe que possibilita acessarmos a propriedade "normalmente"
+    //ex: n1.quantidade, porém se tentarmos alterá-la atribuindo um novo valor o comonado será ignorado,
+    //n1.quantidade = 1000 não seria executado.
+
+    get data() {
+        return this._data;
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+    get volume(){
+        return this._quantidade * this._valor;
     }
 }
+
