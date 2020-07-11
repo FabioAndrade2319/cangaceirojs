faeddbc0dd09972a589bead43a14b40a819c8063
Fabio Andrade <fmandrade2319@gmail.com>
2020-07-11 10:51:56 -0300

    Alterado construtor da data para impedir alterações conf. pag. 43

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -4,7 +4,9 @@ class Negociacao {
     //do underline para deixar claro que as propriedades não devem ser acessadas diretamente.
 
     constructor(data, quantidade, valor) {
-        this._data = data;
+        //a data foi alterada para esse formato como programação defensiva, para evitar que a
+        //data possa ser alterada. ref. pág 43
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
         Object.freeze(this);
@@ -15,7 +17,7 @@ class Negociacao {
     //n1.quantidade = 1000 não seria executado.
 
     get data() {
-        return this._data;
+        return new Date(this._data.getTime());
     }
 
     get quantidade() {
