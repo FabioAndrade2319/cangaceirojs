7a8b52b67ab9a2ab5c6175c3a83f49a0b6661770
Fabio Andrade <fmandrade2319@gmail.com>
2020-08-01 10:27:55 -0300

    Implementa capitulo 4 completo

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -7,11 +7,18 @@ class DateConverter {
 
     //getDate() retorna o dia do mês, getMonth() retorna o mês e getFullYear() retorna o ano com 4 digitos
     static paraTexto (data) {
+
+        
         return (`${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`);
 
     }
 
     static paraData (texto) {
+
+        //condição para testar se a data está no formato correto adotando o conceito de fail fast
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto)) 
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+
         //fazemos uso aqui do "spread operator - ..." que permite passar um array como argumento
         //e os elementos do array serão considerados na ordem.
         return new Date(...texto
